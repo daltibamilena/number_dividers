5b59febdd443351d05033b663bc79e1e4fcb1818
Milena Daltiba <daltibamilena@gmail.com>
2022-03-29 17:37:00 -0400

    feat: add tests

---
+++ test/request/dividers.test.js
@@ -0,0 +1,32 @@
+const app = require("../../app");
+const supertest = require("supertest");
+const request = supertest(app);
+
+describe("Retornar divisores", () => {
+  test("Número 30", async () => {
+    let response = await request.post("/dividers").send({
+      number: 30,
+    });
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({
+      divisores: [1, 2, 3, 5, 6, 10, 15, 30],
+      divisoresPrimos: [1, 2, 3, 5],
+    });
+  });
+
+  test("Número 24.5", async () => {
+    let response = await request.post("/dividers").send({
+      number: 24.5,
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toStrictEqual("Validation failed");
+  });
+
+  test("Número number", async () => {
+    let response = await request.post("/dividers").send({
+      number: "number",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toStrictEqual("Validation failed");
+  });
+});
